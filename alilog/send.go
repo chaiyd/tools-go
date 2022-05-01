4101328a01f5d2eda21349ad6b5b407f35b0da01
chaiyd <chaiyd.cn@gmail.com>
2022-05-01 10:26:43 +0800

    alilog 添加信号捕获，以便程序正常退出

--- alilog/send.go
+++ alilog/send.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/aliyun/aliyun-log-go-sdk/producer"
@@ -21,8 +22,8 @@ func AliSendLog() {
 	producerConfig.AccessKeyID = fmt.Sprint(cfg.Section("server").Key("AccessKeyId"))
 	producerConfig.AccessKeySecret = fmt.Sprint(cfg.Section("server").Key("AccessKeySecret"))
 	producerInstance := producer.InitProducer(producerConfig)
-	ch := make(chan os.Signal)
-	signal.Notify(ch, os.Kill, os.Interrupt)
+	// ch := make(chan os.Signal)
+	// signal.Notify(ch, os.Kill, os.Interrupt)
 	producerInstance.Start() // 启动producer实例
 
 	// 填写日志组名称。
@@ -40,15 +41,28 @@ func AliSendLog() {
 	}
 
 	defer f.Close()
+
 	reader := bufio.NewReader(f)
+	c := make(chan os.Signal)
+	//监听指定信号 ctrl+c kill
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGUSR1, syscall.SIGUSR2)
+
 	for {
-		// line, err := reader.ReadString('\n')
+
+		s := <-c
+		switch s {
+		case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+			producerInstance.SafeClose()
+			fmt.Println("退出程序：", s)
+			os.Exit(0)
+		default:
+			return
+		}
+
 		line, _, err := reader.ReadLine()
 		// fmt.Printf("这一行是：", fmt.Sprintf("%s%v\n", line)
-
 		if err == io.EOF {
 			// fmt.Printf("数据读取完毕")
-			// break
 			time.Sleep(time.Second * 1)
 			continue
 		}
@@ -58,8 +72,7 @@ func AliSendLog() {
 		}
 		log := producer.GenerateLog(uint32(time.Now().Unix()), map[string]string{"content": fmt.Sprintf("%s\n", line)})
 		producerInstance.SendLog(LOGProject, LOGLogstore, LOGTopic, LOGSource, log)
-		// fmt.Println("发送成功")
-		// producerInstance.SafeClose()
-	}
 
+	}
+	// fmt.Println("发送成功")
 }
